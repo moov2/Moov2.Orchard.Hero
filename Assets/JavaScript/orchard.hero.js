fe66967a65dba9d3b8be97b4cddbc83dcb7d5522
Peter Keating <peter@moov2.com>
2017-08-22 14:46:30 +0100

    Create hero via selecting image from media library
    Content editor can use "Create from Media" button to generate markup
    for hero component by selecting an image from Orchard Media Library.
    Support is limited to a single image at the moment, plans to support
    video, third party embeds and multiple images.

---
+++ Assets/JavaScript/orchard.hero.js
@@ -0,0 +1,70 @@
+﻿(function () {
+    var CONTENT_TYPE_IMAGE = 'Image';
+
+    var $openMediaBtn = $('.js-open-media-library'),
+        $textArea = $('#Hero_Text');
+
+    var addListeners = function () {
+        $openMediaBtn.on('click', openMediaPicker);
+    };
+
+    /**
+     * Opens media picker, content editor can select a media from library, which
+     * is then used to generate HTML.
+     */
+    var openMediaPicker = function () {
+        var adminIndex = location.href.toLowerCase().indexOf('/admin/'),
+            cachedScrollPosition = 0;
+
+        if (adminIndex === -1) {
+            return false;
+        }
+
+        $.colorbox({
+            href: location.href.substr(0, adminIndex) + '/Admin/Orchard.MediaLibrary?dialog=true',
+            iframe: true,
+            reposition: true,
+            width: '90%',
+            height: '90%',
+            onLoad: function () {
+                cachedScrollPosition = $('body').scrollTop();
+                // hide the scrollbars from the main window
+                $('html, body').css('overflow', 'hidden');
+            },
+            onClosed: function () {
+                $('html, body').css('overflow', '');
+                $('body').scrollTop(cachedScrollPosition);
+
+                setHtml($.colorbox.selectedData);
+            }
+        });
+
+        return false;
+    };
+
+    /**
+     * Returns HTML for a single image.
+     */
+    var renderSingleImage = function (image) {
+        return '<div class="hero">\n\t<div class="hero__content">\n\t\t<img src="' + image.resource + '" alt="' + image.alternateText + '" />\n\t</div>\n</div>';
+    };
+
+    /**
+     * Uses selected media to set HTML.
+     */
+    var setHtml = function (media) {
+        if (!media || media.length === 0) {
+            return;
+        }
+
+        var html = '';
+
+        if (media.length === 1 && media[0].contentType === CONTENT_TYPE_IMAGE) {
+            html = renderSingleImage(media[0]);
+        }
+
+        $textArea.val(html).trigger('change');
+    };
+
+    addListeners();
+})();
---
+++ ResourceManifest.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.UI.Resources;
+
+namespace Moov2.Orchard.Hero
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest.DefineScript("HeroJs").SetUrl("orchard.hero.min.js", "orchard.hero.js").SetDependencies(new string[] { "jQueryColorBox" });
+        }
+    }
+}
