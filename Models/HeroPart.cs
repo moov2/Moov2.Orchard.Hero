9f120154a9b282fb636184fcb5dd5b74877309c7
Peter Keating <peter@moov2.com>
2017-10-02 15:30:27 +0100

    Add ability to manage settings for HeroPart
    Added ability to manage HeroPart settings, starting with customising
    the instructions shown when editing a hero. Also, added the ability
    to define the breakpoints that should be used when generating the hero
    HTML via selecting a media element.

--- Models/HeroPart.cs
+++ Models/HeroPart.cs
@@ -1,9 +1,12 @@
-﻿using Orchard.ContentManagement;
+﻿using Moov2.Orchard.Hero.Settings;
+using Orchard.ContentManagement;
 
 namespace Moov2.Orchard.Hero.Models
 {
     public class HeroPart : ContentPart<HeroPartRecord>
     {
+        #region PartData
+
         public string Html
         {
             get { return Retrieve(x => x.Html); }
@@ -15,5 +18,28 @@ namespace Moov2.Orchard.Hero.Models
             get { return Html; }
             set { Html = value; }
         }
+
+        #endregion
+
+        #region PartSettings
+
+        public string Breakpoints
+        {
+            get
+            {
+                return TypePartDefinition.Settings.GetModel<HeroTypePartSettings>().Breakpoints;
+            }
+        }
+
+        public string Hint
+        {
+            get
+            {
+                return TypePartDefinition.Settings.GetModel<HeroTypePartSettings>().Hint;
+            }
+        }
+
+        #endregion
+
     }
 }
---
+++ Settings/HeroSettings.cs
@@ -0,0 +1,45 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.ContentManagement.MetaData.Models;
+using Orchard.ContentManagement.ViewModels;
+using System.Collections.Generic;
+
+namespace Moov2.Orchard.Hero.Settings
+{
+    public class HeroTypePartSettings
+    {
+        public string Hint { get; set; }
+
+        public string Breakpoints { get; set; }
+    }
+
+    public class HeroSettingsHooks : ContentDefinitionEditorEventsBase
+    {
+        private const string DefaultBreakpoints = "450, 768, 1400, 1900";
+        private const string DefaultHint = @"<p>The hero section is a full-width media item displayed before the page content.</p>
+<p>Use the shortcut below to quickly generate HTML for the hero section by picking a media item from the media library.</p>
+<p>Alternatively, you can customise the display of a hero section by constructing custom HTML using the editor.</p>";
+
+        public override IEnumerable<TemplateViewModel> TypePartEditor(ContentTypePartDefinition definition)
+        {
+            if (definition.PartDefinition.Name != "HeroPart")
+                yield break;
+
+            var model = definition.Settings.GetModel<HeroTypePartSettings>();
+            yield return DefinitionTemplate(model);
+        }
+
+        public override IEnumerable<TemplateViewModel> TypePartEditorUpdate(ContentTypePartDefinitionBuilder builder, IUpdateModel updateModel)
+        {
+            if (builder.Name != "HeroPart")
+                yield break;
+
+            var model = new HeroTypePartSettings();
+            updateModel.TryUpdateModel(model, "HeroTypePartSettings", null, null);
+            builder.WithSetting("HeroTypePartSettings.Hint", !string.IsNullOrWhiteSpace(model.Hint) ? model.Hint : DefaultHint);
+            builder.WithSetting("HeroTypePartSettings.Breakpoints", !string.IsNullOrWhiteSpace(model.Breakpoints) ? model.Breakpoints : DefaultBreakpoints);
+            yield return DefinitionTemplate(model);
+        }
+    }
+}
